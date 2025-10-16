5e0dd28a33fe69d909442b5f5eeca6bce6ac8437
Ivana Nikolic <ivananikolic@Ivanas-MacBook-Air.local>
2025-10-16 15:11:44 +0200

    prva lekcija

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <title>Demo</title>
+</head>
+<body>
+    <h1> 
+        <?php  
+            echo "Cao";
+        ?>
+    </h1>
+    
+</body>
+</html>
