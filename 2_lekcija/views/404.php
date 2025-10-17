8ea0d4a79d79f8ed6bbf1e2cdda12431defe9715
Ivana Nikolic <ivananikolic@Ivanas-MacBook-Air.local>
2025-10-17 11:05:08 +0200

    2_lekcija

---
+++ 2_lekcija/views/404.php
@@ -0,0 +1,25 @@
+
+<?php require('partial/head.php') ?>
+
+<?php require('partial/nav.php') ?>
+
+
+
+<main>
+
+    <div>
+        <h1> Sorry not found</h1>
+         <p>
+
+            <a href="/" > Go back </a>
+         </p>
+
+
+
+    </div>
+
+
+
+
+</main>
+<?php require('partial/footer.php') ?>
